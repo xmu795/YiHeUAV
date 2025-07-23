17885ba162ebcc093236fa2c4cc31e72dcb66579
FallThrive <fallthrive@outlook.com>
2025-07-24 02:36:43 +0800

    修改opencv和uav_bt功能包配置文件，避免编译报错

--- src/opencv/setup.py
+++ src/opencv/setup.py
@@ -17,7 +17,7 @@ setup(
     maintainer_email='fallthrive@outlook.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    # tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'circle_centre_node = opencv.circle_centre_node:main'
